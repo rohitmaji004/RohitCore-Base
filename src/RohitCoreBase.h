bd1bae77ce2e9c900410ca337d621605e41c1d79
rohitmaji004 <153757813+rohitmaji004@users.noreply.github.com>
2026-03-26 15:10:10 +0530

    Update RohitCoreBase.h

--- src/RohitCoreBase.h
+++ src/RohitCoreBase.h
@@ -1,1 +1,24 @@
+#ifndef RohitCoreBase_h
+#define RohitCoreBase_h
 
+#include <Arduino.h>
+
+enum RCLogLevel {
+  RC_INFO,
+  RC_WARN,
+  RC_ERROR
+};
+
+class RohitCoreBase {
+  public:
+    static void begin(bool enableDebug = true, long baud = 115200);
+    static void log(RCLogLevel level, String message);
+    static void debug(String message);
+    static String getBoard();
+    static uint32_t getFreeMemory();
+
+  private:
+    static bool _debugEnabled;
+};
+
+#endif
